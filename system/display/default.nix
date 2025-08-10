ae4de3d38c9aeb4815ebe3d59d9b5e26661e3ddd
nixple <default_value@outlook.com>
2025-08-10 19:45:02 +0800

    up

--- system/display/default.nix
+++ system/display/default.nix
@@ -49,8 +49,8 @@
 
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
   
 
   # Configure keymap in X11
